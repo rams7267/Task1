dca2becd8bb3ca3f1425a8ae5847c5c717b2f9c6
rams7267 <avinashkondapalli2000@gmail.com>
2020-01-15 18:31:14 +0530

    Updates

---
+++ precache-manifest.31534841ecc56266864b5d60cf747113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13eae0eb0f4bbcf74b83fea4849016e0",
+    "url": "/task1/index.html"
+  },
+  {
+    "revision": "4b14d9d62adb91cf6895",
+    "url": "/task1/static/css/main.297785f3.chunk.css"
+  },
+  {
+    "revision": "56f328982ed03c13f87d",
+    "url": "/task1/static/js/2.df30a171.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/task1/static/js/2.df30a171.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b14d9d62adb91cf6895",
+    "url": "/task1/static/js/main.4bb54b61.chunk.js"
+  },
+  {
+    "revision": "268bee412bb9093318bc",
+    "url": "/task1/static/js/runtime-main.c5c8bf54.js"
+  }
+]);
